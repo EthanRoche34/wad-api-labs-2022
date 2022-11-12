181c4316bcac356cf28d400de6d8a9ab107caefd
Ethan Roche Woodward <ethanroche17@gmail.com>
2022-11-12 21:08:22 +0000

    added routing for movies and movie reviews

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+
 import moviesRouter from "./api/movies";
 
 dotenv.config();
@@ -7,6 +8,8 @@ dotenv.config();
 const app = express();
 
 const port = process.env.PORT;
+
+app.use(express.json());
 
 app.use("/api/movies", moviesRouter);
 
