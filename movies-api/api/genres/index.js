779fecc9cc1b312a3ad707ce66feb93c45773136
Ethan Roche Woodward <ethanroche17@gmail.com>
2022-11-18 15:59:13 +0000

    Mongo lab Exercises

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,8 +1,13 @@
 import express, { application } from "express";
-import { genres } from "./genresData";
+import Genre from "./genreModel";
+import asyncHandler from "express-async-handler";
 
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.json(genres);
+
+// Get all Genres
+router.get("/", async (req, res) => {
+  const genres = await Genre.find();
+  res.status(200).json(genres);
 });
+
 export default router;
